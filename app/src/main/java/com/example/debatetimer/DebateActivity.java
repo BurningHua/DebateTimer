8b2b347a7c9f60dbbef0394ec989a4db49a325e7
BurningHua <352173463@qq.com>
2019-03-26 00:31:15 +0800

    final project

--- app/src/main/java/com/example/debatetimer/DebateActivity.java
+++ app/src/main/java/com/example/debatetimer/DebateActivity.java
@@ -1,17 +1,103 @@
 package com.example.debatetimer;
 
+import android.annotation.SuppressLint;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class DebateActivity extends AppCompatActivity {
     private String ipAddress;
-    private boolean isStart;
+    private int isStart;
     private int recLen;
-    private int period;
+    private int period = 0;
+    private TextView txtTime,txtPeriod;
+    private CountDownTimer timer;
+    private String timerState;
     private String periodName[];
+    private Socket mSocket;
+    private OutputStream mOutStream;
+    private InputStream mInStream;
+
+    //private SocketConnectThread socketConnectThread;
+
+    private StringBuffer sb;
+
+    @SuppressLint("HandlerLeak")
+
+    public Handler handler = new Handler(){
+
+        @Override
+
+        public void handleMessage(Message msg) {
+
+            super.handleMessage(msg);
+
+            if (msg.what == 1){
+                sb = new StringBuffer("");
+                if(timer!=null){
+                    timer.cancel();
+                }
+
+                Bundle data = msg.getData();
+
+                sb.append(data.getString("msg"));
+                Log.i("servermsg", sb.toString());
+                timerState = sb.toString();
+                period = Integer.parseInt(timerState.substring(0,1));
+                isStart = Integer.parseInt(timerState.substring(1,2));
+                recLen = Integer.parseInt(timerState.substring(2,5));
+                txtPeriod.setText(periodName[period]);
+                if(isStart == 0){
+                    txtTime.setText(recLen+" seconds");
+                }
+                else{
+                    timer = new CountDownTimer(recLen * 1000, 1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            txtTime.setText((recLen - 1) + " seconds");
+                            recLen--;
+                            if (recLen <= 0) {
+                                recLen = 0;
+                            }
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            isStart = 0;
+                            txtTime.setText("Time is up!");
+                        }
+                    };
+                    timer.start();
+                }
+                if(period == 5){
+                    txtTime.setText("Debate is over!");
+                    txtPeriod.setText(" ");
+                }
+
+
+                //sb.append("\n");
+
+                //tv_msg.setText(sb.toString());
+
+            }
+
+        }
+
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +105,12 @@ public class DebateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_debate);
         ipAddress = getIntent().getStringExtra("ipaddress");
         Log.i("tag", ipAddress);
+        txtTime = findViewById(R.id.textViewTime);
+        txtPeriod = findViewById(R.id.textViewPeriod);
+        SocketConnectThread socketConnectThread = new SocketConnectThread();
+        socketConnectThread.start();
+
+        //Toast.makeText(DebateActivity.this, "The IP address is invalid.", Toast.LENGTH_SHORT).show();
 
         periodName=new String[6];
         periodName[0]="Opening statement from the pro:";
@@ -28,12 +120,96 @@ public class DebateActivity extends AppCompatActivity {
         periodName[4]="Closing statement from the pro:";
         periodName[5]="Closing statement from the pro:";
 
+
+
         Button btnExit =(Button)findViewById(R.id.btnExit);
         btnExit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+               finish();
+
             }
         });
     }
+
+
+    class SocketConnectThread extends Thread{
+        @Override
+        public void run(){
+            Log.e("info", "run" );
+            try {
+                mSocket = new Socket(ipAddress, 12345);
+               // txtTime.setText("REST TIME");
+                if(mSocket != null){
+                    mOutStream = mSocket.getOutputStream();
+                    mInStream = mSocket.getInputStream();
+                }else {
+                    Toast.makeText(DebateActivity.this, "The IP address is invalid.", Toast.LENGTH_SHORT).show();
+                    Log.e("info", "run: =========scoket==null");
+                }
+            } catch (Exception e) {
+                Log.i("info", "invalid address");
+                //Toast.makeText(DebateActivity.this, "Invalid address.", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+                return;
+            }
+            Log.e("info","connect success========================================");
+            startReader(mSocket);
+        }
+    }
+
+    private void startReader(final Socket socket) {
+
+        new Thread(){
+
+            @Override
+
+            public void run() {
+
+                DataInputStream reader;
+
+                try {
+
+                    reader = new DataInputStream(socket.getInputStream());
+
+                    while (true) {
+
+                        String msg = reader.readUTF();
+                        Message message = new Message();
+                        message.what = 1;
+                        Bundle bundle = new Bundle();
+                        bundle.putString("msg", msg);
+                        message.setData(bundle);
+                        handler.sendMessage(message);
+
+
+                    }
+
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+
+                }
+
+            }
+
+        }.start();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/example/debatetimer/RefereeActivity.java
+++ app/src/main/java/com/example/debatetimer/RefereeActivity.java
@@ -1,34 +1,91 @@
 package com.example.debatetimer;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class RefereeActivity extends AppCompatActivity {
     private String ipAddress;
-    private boolean isStart = false;
+    private int isStart = 0;
     private int recLen;
     private int period = 0;
-    private Button btnNext,btnStart,btnStop,btnRefresh;
-    private TextView txtIP,txtPeriod,txtTime;
-    private int timeOpen,timeFree,timeClose;
+    private Button btnNext, btnStart, btnStop, btnRefresh;
+    private TextView txtIP, txtPeriod, txtTime;
+    private int timeOpen, timeFree, timeClose;
     private String periodName[];
     private int timeArr[];
     private CountDownTimer timer;
     private boolean startflag = false;
+    private String msgToSend;
+    private ServerSocket mServerSocket;
+    private Socket mSocket;
+    private StringBuffer sb = new StringBuffer();
+    private OutputStream mOutStream;
+    private DataOutputStream writer;
+
+    @SuppressLint("HandlerLeak")
+    public Handler handler = new Handler() {
+
+        @Override
+
+        public void handleMessage(Message msg) {
+
+            super.handleMessage(msg);
+
+            if (msg.what == 1) {
+
+                Bundle data = msg.getData();
+
+                sb.append(data.getString("msg"));
+
+                sb.append("\n");
+
+                //tv_msg.setText(sb.toString());
+
+            }
+
+        }
+
+    };
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_referee);
+
+        try {
+
+            mServerSocket = new ServerSocket(12345);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+
+        SocketAcceptThread socketAcceptThread = new SocketAcceptThread();
+
+        socketAcceptThread.start();
 
         txtPeriod = findViewById(R.id.textView);
         txtTime = findViewById(R.id.textViewTime);
@@ -41,41 +98,46 @@ public class RefereeActivity extends AppCompatActivity {
         txtIP.setText("Your IP address is: " + getlocalip());
 
 
-        periodName=new String[6];
-        periodName[0]="Opening statement from the pro";
-        periodName[1]="Opening statement from the con";
-        periodName[2]="Free debate";
-        periodName[3]="Closing statement from the con";
-        periodName[4]="Closing statement from the pro";
-        periodName[5]="Closing statement from the pro";
+        periodName = new String[6];
+        periodName[0] = "Opening statement from the pro";
+        periodName[1] = "Opening statement from the con";
+        periodName[2] = "Free debate";
+        periodName[3] = "Closing statement from the con";
+        periodName[4] = "Closing statement from the pro";
+        periodName[5] = "Closing statement from the pro";
 
         timeArr = new int[6];
-        timeArr[0]=getIntent().getIntExtra("timeofopen",180);
-        timeArr[1]=timeArr[0];
-        timeArr[2]=getIntent().getIntExtra("timeoffree",480);
-        timeArr[3]=getIntent().getIntExtra("timeofclose",150);
-        timeArr[4]=timeArr[3];
-        timeArr[5]=timeArr[4];
+        timeArr[0] = getIntent().getIntExtra("timeofopen", 180);
+        timeArr[1] = timeArr[0];
+        timeArr[2] = getIntent().getIntExtra("timeoffree", 480);
+        timeArr[3] = getIntent().getIntExtra("timeofclose", 150);
+        timeArr[4] = timeArr[3];
+        timeArr[5] = timeArr[4];
         recLen = timeArr[0];
 
-        txtTime.setText(""+timeArr[period]+" seconds");
+        txtTime.setText("" + timeArr[period] + " seconds");
 
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(timer!=null){
+                if (timer != null) {
                     timer.cancel();
                 }
                 period++;
                 recLen = timeArr[period];
+                isStart = 0;
                 txtPeriod.setText(periodName[period]);
-                txtTime.setText(""+timeArr[period]+" seconds");
-                if(period == 4){
+                txtTime.setText("" + timeArr[period] + " seconds");
+                if (period == 4) {
                     btnNext.setText("EXIT");
                 }
-                if(period == 5){
+                if (period == 5) {
+                    msgToSend = String.valueOf(period) + String.valueOf(isStart) + String.format("%03d", recLen);
+                    send(msgToSend);
                     finish();
                 }
+                msgToSend = String.valueOf(period) + String.valueOf(isStart) + String.format("%03d", recLen);
+                send(msgToSend);
                 startflag = false;
             }
         });
@@ -83,25 +145,29 @@ public class RefereeActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(startflag == false){
-                    isStart = true;
-                    timer = new CountDownTimer(recLen*1000, 1000) {
+                if (startflag == false) {
+                    isStart = 1;
+                    timer = new CountDownTimer(recLen * 1000, 1000) {
                         @Override
                         public void onTick(long millisUntilFinished) {
-                            txtTime.setText((recLen-1) + " seconds");
+                            txtTime.setText((recLen - 1) + " seconds");
                             recLen--;
-                            if(recLen<=0){
+                            if (recLen <= 0) {
                                 recLen = 0;
                             }
                         }
+
                         @Override
                         public void onFinish() {
-                            isStart = false;
+                            isStart = 0;
                             txtTime.setText("Time is up!");
                         }
                     };
                     timer.start();
-                    startflag=true;
+                    startflag = true;
+                    msgToSend = String.valueOf(period) + String.valueOf(isStart) + String.format("%03d", recLen);
+                    send(msgToSend);
+                    Log.i("message", msgToSend);
                 }
 
             }
@@ -110,12 +176,14 @@ public class RefereeActivity extends AppCompatActivity {
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(timer!=null){
+                if (timer != null) {
                     timer.cancel();
                 }
-                txtTime.setText(""+recLen+" seconds");
-                isStart = false;
+                txtTime.setText("" + recLen + " seconds");
+                isStart = 0;
                 startflag = false;
+                msgToSend = String.valueOf(period) + String.valueOf(isStart) + String.format("%03d", recLen);
+                send(msgToSend);
 
             }
         });
@@ -123,19 +191,23 @@ public class RefereeActivity extends AppCompatActivity {
         btnRefresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(timer!=null){
+                if (timer != null) {
                     timer.cancel();
                 }
                 recLen = timeArr[period];
-                txtTime.setText(""+timeArr[period]+" seconds");
+                txtTime.setText("" + timeArr[period] + " seconds");
+                isStart = 0;
                 startflag = false;
+                msgToSend = String.valueOf(period) + String.valueOf(isStart) + String.format("%03d", recLen);
+                send(msgToSend);
 
             }
         });
     }
 
     private String getlocalip() {
-        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);;
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        ;
         WifiInfo wifiInfo = wifiManager.getConnectionInfo();
         int ipAddress = wifiInfo.getIpAddress();
         //  Log.d(Tag, "int ip "+ipAddress);
@@ -144,11 +216,133 @@ public class RefereeActivity extends AppCompatActivity {
                 + (ipAddress >> 16 & 0xff) + "." + (ipAddress >> 24 & 0xff));
     }
 
+    class SocketAcceptThread extends Thread {
+
+        @Override
+
+        public void run() {
+
+            try {
+                mSocket = mServerSocket.accept();
+               // mOutStream = mSocket.getOutputStream();
+                writer = new DataOutputStream(mSocket.getOutputStream());
+                msgToSend = String.valueOf(period) + String.valueOf(isStart) + String.format("%03d", recLen);
+                send(msgToSend);
+
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+                Log.i("info", "run: ==============" + "accept error");
+
+                return;
+
+            }
+
+            Log.i("info", "accept success==================");
+
+            //startReader(mSocket);
+
+        }
+
+    }
+
+    private void startReader(final Socket socket) {
+
+        new Thread() {
+
+            @Override
+
+            public void run() {
+
+                DataInputStream reader;
+
+                try {
+
+                    reader = new DataInputStream(socket.getInputStream());
+
+                    while (true) {
+
+                        String msg = reader.readUTF();
+
+                        //DataOutputStream writer = new DataOutputStream(mSocket.getOutputStream());
+
+                        //writer.writeUTF("收到：" + msg);
+
+                        Message message = new Message();
+
+                        message.what = 1;
+
+                        Bundle bundle = new Bundle();
+
+                        bundle.putString("msg", msg);
+
+                        message.setData(bundle);
+
+                        handler.sendMessage(message);
+
+                    }
+
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+
+                }
+
+            }
+
+        }.start();
+
+    }
+
+    public void send(final String str) {
+
+        if (str.length() == 0) {
+
+            return;
+
+        }
+        new Thread() {
+
+            @Override
+
+            public void run () {
+
+                try {   //发送
+                    writer.writeUTF(str);
+                    writer.flush();
+                    //mOutStream.flush();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }.start();
+
+    }
+
+
+
+
+
+
+
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        if (mServerSocket != null){
+            try {
+                mServerSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         if (timer != null) {
             timer.cancel();
             timer = null;
